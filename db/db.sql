127e931c2bd46889cdba708d99c4ccc466b79229
Paolo Bianchessi <paolo.bianchessi05@gmail.com>
2023-12-17 22:44:08 +0100

    Added every table for MySQL database

--- db/db.sql
+++ db/db.sql
@@ -32,3 +32,17 @@ CREATE TABLE includes(
     FOREIGN KEY (eventId) REFERENCES events(id),
     FOREIGN KEY (mediaId) REFERENCES medias(id)
 );
+
+CREATE TABLE categories(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    description TEXT
+);
+
+CREATE TABLE eventsCategories(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    eventId INT,
+    categoryId INT,
+    FOREIGN KEY (eventId) REFERENCES events(id),
+    FOREIGN KEY (categoryId) REFERENCES categories(id)
+);
