4852331b24e199d84258c102315fded3cb3998b4
Paolo Bianchessi <paolo.bianchessi05@gmail.com>
2024-02-20 21:15:55 +0100

    Aggiunta una nuova libreria che si occupa dei range di date, modificato il modello dei post.

--- models/postModel.ts
+++ models/postModel.ts
@@ -1,23 +1,77 @@
-// Paolo Bianchessi, 29/10/2023
+// Andrea Polli, 15/02/2024
 // The following code provides the methods about the posts
 
+import { PostType } from "@/types/postType";
 import { db } from "./db";
+import { RowDataPacket } from "mysql2";
 
 /**
  * This object is responsible for all the db interaction methods
  * about the Post
  */
 const Post = {
-  create: async (data: string, authorId: number) => {
-    //too do
+  /**
+   * Creating a post defining all its values.
+   */
+  createPost: async (post: PostType): Promise<boolean> => {
+    await db
+      .promise()
+      .query(
+        "INSERT INTO admins (title, description, actualDate, startDate, endDate, creatorId, lastModificatorId) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        [
+          post.title,
+          post.description,
+          post.actualDate,
+          post.startDate,
+          post.endDate,
+          post.creatorId,
+          post.creatorId,
+        ]
+      );
+
+    return true;
   },
-  delete: async (id: number) => {
-    // to  do
+  delete: async (id: number): Promise<void> => {
+    await db.promise().query("DELETE FROM posts WHERE id = ?", [id]);
   },
-  edit: async (id: number, newData: string, editorId: number) => {
-    // to do
+  edit: async (
+    id: number,
+    title: string,
+    description: string,
+    actualDate: Date,
+    startDate: string,
+    endDate: string,
+    lastModificatorId: number
+  ): Promise<boolean> => {
+    await db
+      .promise()
+      .query<RowDataPacket[]>(
+        "UPDATE categories SET title = ?, description = ?, actualDate = ?, startDate = ?, endDate = ?, lastModificatorId = ? WHERE id = ?",
+        [
+          title,
+          description,
+          actualDate,
+          startDate,
+          endDate,
+          lastModificatorId,
+          id,
+        ]
+      );
+    return true;
   },
-  getAllPosts: async () => {
-    // to do
+  getNextFiveShort: async (): Promise<Array<PostType> | null> => {
+    const [rows] = await db
+      .promise()
+      .query(
+        "SELECT title, actualDate, startDate, endDate FROM posts WHERE actualDate >= CURDATE() ORDER BY actualDate LIMIT 5"
+      );
+    return rows as PostType[] | null;
+  },
+
+  listAll: async (): Promise<PostType[]> => {
+    const [rows] = await db
+      .promise()
+      .query<RowDataPacket[]>("SELECT * FROM posts");
+    return rows as PostType[];
   },
 };
