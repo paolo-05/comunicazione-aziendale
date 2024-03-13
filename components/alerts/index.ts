81023eacac885eeff3cc0ecc6c176d48da3f90c8
Paolo Bianchessi <paolo.bianchessi05@gmail.com>
2024-03-13 23:17:16 +0100

    updated /components/alerts/index.ts

--- components/alerts/index.ts
+++ components/alerts/index.ts
@@ -1,14 +1,2 @@
-// Info Alert
-export * from './infoAlert';
-
 // Danger Alert
-export * from './dangerAlert';
-
-// Success Alert
-export * from './successAlert';
-
-// Warning Alert
-export * from './warningAlert';
-
-// General Alert
-export * from './generalAlert';
+export * from "./dangerAlert";
